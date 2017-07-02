7a4fc027c73d0b209275a762f41a2547f8e87280
Kardaev Mikhail <Kardaev.74@gmail.com>
2017-07-02 22:28:07 +0500

    first commit

---
+++ src/main/java/ru/mdkardaev/projectstatistic/ProjectStatisticApplication.java
@@ -0,0 +1,12 @@
+package ru.mdkardaev.projectstatistic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectStatisticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectStatisticApplication.class, args);
+	}
+}
---
+++ src/test/java/ru/mdkardaev/projectstatistic/ProjectStatisticApplicationTests.java
@@ -0,0 +1,16 @@
+package ru.mdkardaev.projectstatistic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProjectStatisticApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
