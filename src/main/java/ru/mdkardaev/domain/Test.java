45d882dc235de07b4152bc05552685aa3769f5f7
Kardaev Mikhail <Kardaev.74@gmail.com>
2017-07-16 22:21:24 +0500

    add flyway

--- src/main/java/ru/mdkardaev/domain/Test.java
+++ src/main/java/ru/mdkardaev/domain/Test.java
@@ -1,15 +1,10 @@
 package ru.mdkardaev.domain;
 
 import lombok.Data;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import javax.persistence.*;
 
-/**
- * Created by Mihail on 10.07.2017.
- */
 @Entity
 @Table(name = "Test")
 @Data
@@ -17,7 +12,7 @@ import javax.persistence.*;
 public class Test {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue
     private long id;
 
     @Column(name = "testString")
---
+++ src/main/resources/db/migration/V1__base.sql
@@ -0,0 +1,6 @@
+CREATE TABLE test(
+  id bigint NOT NULL,
+  test_string character varying(255),
+
+  CONSTRAINT test_pkey PRIMARY KEY (id)
+);
