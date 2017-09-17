4dab48f604595a25d08755cbb03bd91dad887044
Kardaev Mikhail <Kardaev.74@gmail.com>
2017-09-17 12:26:38 +0500

    add cors

--- src/main/java/ru/mdkardaev/Application.java
+++ src/main/java/ru/mdkardaev/Application.java
@@ -3,7 +3,11 @@ package ru.mdkardaev;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 @EnableTransactionManagement
@@ -11,5 +15,16 @@ public class Application {
 
     public static void main(String[] args) throws JsonProcessingException {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                //Добавляет cors для webpack-server
+                registry.addMapping("/**").allowedOrigins("http://localhost:8888");
+            }
+        };
     }
 }
