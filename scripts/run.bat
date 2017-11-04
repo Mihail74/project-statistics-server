c00761cef911e4f8882197408cf20215b47b6da2
Kardaev Mikhail <Kardaev.74@gmail.com>
2017-11-04 11:43:07 +0500

    run.bar

--- scripts/run.bat
+++ scripts/run.bat
@@ -1,1 +1,9 @@
-test
+@ECHO off
+
+set postgresURL=jdbc:postgresql://localhost:5432/project-statistics
+set /p postgresURL=Postgres url [%postgresURL%]:
+
+set loggingFile=app.log
+set /p loggingFile="Log file [%loggingFile%]: "
+
+java -Dspring.datasource.url=%postgresURL% -Dlogging.file=%loggingFile% -jar project-statistics-server-1.0.0.jar
